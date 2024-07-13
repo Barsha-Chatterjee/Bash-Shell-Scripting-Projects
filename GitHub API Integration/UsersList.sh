45459909b14f9ee6e238b52afbe6b8c7cb7686df
Barsha Chatterjee <barshachatterjee427@gmail.com>
2024-07-13 20:27:55 +0530

    Update UsersList.sh

--- GitHub API Integration/UsersList.sh
+++ GitHub API Integration/UsersList.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+
+# GitHub API URL
+GitHub_API_URL="https://api.github.com"
+
+# Enter your username and API access Token
+Username=$username
+Token=$token
+
+# Owner and Repo information
+Repo_owner=$1
+Repo_name=$2
+
+# Sending HTTPS GET request to GitHub API
+function github_api_call {
+local repo_path="$1"
+local url="${GitHub_API_URL}/${repo}"
+
+# Send a GET request to the GitHub API with authentication
+  curl -s -u "${Username}:${Token}" "$url"
+}
+
+# TO BE CONTINUED......!
+
+
 
